df6f95ecf7d94a6f1cea9d2f98f1f70ca6802551
kradyfatty <67622951+kradyfatty@users.noreply.github.com>
2021-03-19 14:38:06 +0800

    haha

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,25 +24,25 @@ window.axios.defaults.withCredentials = true;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 
-import 'vue-toast-notification/dist/theme-sugar.css';
-import '@mdi/font/css/materialdesignicons.css'
-import 'boxicons/css/boxicons.css'
+// import 'vue-toast-notification/dist/theme-sugar.css';
+// import '@mdi/font/css/materialdesignicons.css'
+// import 'boxicons/css/boxicons.css'
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
+// /**
+//  * Echo exposes an expressive API for subscribing to channels and listening
+//  * for events that are broadcast by Laravel. Echo and event broadcasting
+//  * allows your team to easily build robust real-time web applications.
+//  */
 
-import Echo from 'laravel-echo';
+// import Echo from 'laravel-echo';
 
-window.Pusher = require('pusher-js');
+// window.Pusher = require('pusher-js');
 
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    //forceTLS: true
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-});
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     //forceTLS: true
+//     wsHost: window.location.hostname,
+//     wsPort: 6001,
+// });
