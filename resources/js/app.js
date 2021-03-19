98bcc10dbd60b93d95e009f0c9d193e9aaa5ee62
kradyfatty <67622951+kradyfatty@users.noreply.github.com>
2021-03-19 14:49:22 +0800

    wew

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,17 +6,17 @@
 
 require('./bootstrap');
 
-// window.Vue = require('vue').default;
-// import VueTimeago from 'vue-timeago';
-// Vue.use(VueTimeago, {
-//     name: 'timeago', // component name, `timeago` by default
-//     locale: 'en-US',
-//     locales: {
-//         'en-US': require('vue-timeago/locales/en-US.json')
-//     }
-// })
-// import VueToast from 'vue-toast-notification';
-// Vue.use(VueToast);
+window.Vue = require('vue').default;
+import VueTimeago from 'vue-timeago';
+Vue.use(VueTimeago, {
+    name: 'timeago', // component name, `timeago` by default
+    locale: 'en-US',
+    locales: {
+        'en-US': require('vue-timeago/locales/en-US.json')
+    }
+})
+import VueToast from 'vue-toast-notification';
+Vue.use(VueToast);
 
 /**
  * The following block of code may be used to automatically register your
@@ -29,22 +29,22 @@ require('./bootstrap');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 
-// Vue.component('chatbox', require('./components/Chatbox/Chatbox.vue').default);
-// Vue.component('contact-list', require('./components/Chatbox/ContactList.vue').default);
-// Vue.component('chat-conversation', require('./components/Chatbox/Conversation.vue').default);
+Vue.component('chatbox', require('./components/Chatbox/Chatbox.vue').default);
+Vue.component('contact-list', require('./components/Chatbox/ContactList.vue').default);
+Vue.component('chat-conversation', require('./components/Chatbox/Conversation.vue').default);
 
-// Vue.component('articles', require('./components/admin/Articles.vue').default);
-// Vue.component('agencies', require('./components/admin/Agencies.vue').default);
-// Vue.component('staffs', require('./components/admin/Staff.vue').default);
-// Vue.component('dropdownlists', require('./components/admin/Dropdownlists.vue').default);
-// Vue.component('testservices', require('./components/admin/Testservices.vue').default);
-// Vue.component('packages', require('./components/admin/Packages.vue').default);
-// Vue.component('addons', require('./components/admin/Addon.vue').default);
-// Vue.component('sidebar', require('./components/admin/Sidebar.vue').default);
-// Vue.component('methodreference', require('./components/admin/Method.vue').default);
+Vue.component('articles', require('./components/admin/Articles.vue').default);
+Vue.component('agencies', require('./components/admin/Agencies.vue').default);
+Vue.component('staffs', require('./components/admin/Staff.vue').default);
+Vue.component('dropdownlists', require('./components/admin/Dropdownlists.vue').default);
+Vue.component('testservices', require('./components/admin/Testservices.vue').default);
+Vue.component('packages', require('./components/admin/Packages.vue').default);
+Vue.component('addons', require('./components/admin/Addon.vue').default);
+Vue.component('sidebar', require('./components/admin/Sidebar.vue').default);
+Vue.component('methodreference', require('./components/admin/Method.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -28,21 +28,21 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // import '@mdi/font/css/materialdesignicons.css'
 // import 'boxicons/css/boxicons.css'
 
-// /**
-//  * Echo exposes an expressive API for subscribing to channels and listening
-//  * for events that are broadcast by Laravel. Echo and event broadcasting
-//  * allows your team to easily build robust real-time web applications.
-//  */
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
 
-// import Echo from 'laravel-echo';
+import Echo from 'laravel-echo';
 
-// window.Pusher = require('pusher-js');
+window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     //forceTLS: true
-//     wsHost: window.location.hostname,
-//     wsPort: 6001,
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    //forceTLS: true
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+});
